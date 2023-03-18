d24df0b23fdb2711d16f1d350570de1b5027fbb6
Iain Harrison <iainjharrison@gmail.com>
2023-03-18 10:33:12 +0100

    BugOrderUI

---
+++ Assets/DragAndDrop/Examples/ExampleScripts/CharmsArrayUI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DragAndDrop;
+
+public class CharmsArrayUI : ObjectContainerArray
+{
+
+    // where the data comes from - this represents the player, and either their backpack or belt
+    public bool BeEmpty = false;
+    public GameObject StartButton;
+    public Player player;
+    public Player.CharmList charmList;
+
+    public Charm.CharmType charmType = Charm.CharmType.All;
+    public Text description;
+
+    void Start()
+    {
+        if (!BeEmpty)
+        {
+            CreateSlots(player.GetCharms(charmList));
+        }
+        else
+        {
+            CreateSlots(new Charm[player.GetCharms(charmList).Length]);
+        }
+    }
+
+    bool amFull = false;
+    private void FixedUpdate()
+    {
+        if (BeEmpty)
+        {
+            foreach (Transform child in transform)
+            {
+                if (child != null && child.childCount > 0)
+                {
+                    if (!child.GetChild(0).gameObject.active)
+                    {
+                        if (amFull)
+                        {
+                            amFull = false;
+                            StartButton.SetActive(false);
+                        }
+                        return;
+                    }
+                }
+            }
+            if (!amFull)
+            {
+                StartButton.SetActive(true);
+                amFull = true;
+                Debug.Log(amFull);
+            }
+        }
+    }
+
+    // to be able to drop, it must be a charm of the appropriate type
+    public override bool CanDrop(Draggable dragged, Slot slot)
+    {
+        CharmUI charm = dragged as CharmUI;
+
+        // must be a charm
+        if (charm == null)
+            return false;
+
+        Charm ch = charm.obj as Charm;
+
+        // replacing empty slots in here is OK
+        if (ch == null)
+            return true;
+
+        // check the type of items in the belt vs what we can hold
+        int mask = (int)ch.charmType & (int)charmType;
+        return mask != 0;
+    }
+
+    public override void OnDragBegin()
+    {
+        // when we start to drag an object, show its name and requirements in the text field provided
+        base.OnDragBegin();
+        if (description)
+        {
+            CharmUI charmUi = Draggable.current as CharmUI;
+            if (charmUi)
+            {
+                Charm charm = charmUi.obj as Charm;
+                if (charm)
+                    description.text = charm.GetDescription();
+            }
+        }
+    }
+}
---
+++ Assets/DragAndDrop/Examples/ExampleScripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level;
+
+    public CharmsArrayUI LevelBugs;
+    public CharmsArrayUI UseBugs;
+
+    public Charm[] Level1 = new Charm[1];
+    public Charm[] Level2 = new Charm[3];
+    public Charm[] Level3 = new Charm[3];
+    public Charm[] Level4 = new Charm[6];
+
+    public List<Power> powers = new List<Power>(4);
+
+    [Header("Equipped Items")]
+    public Charm helmet;
+    public Charm amulet;
+    public Charm ring1;
+    public Charm ring2;
+    public Charm gloves;
+    public Charm boots;
+    public Charm armour;
+
+    // accessor stuff for UI to use
+    public enum CharmList
+    {
+        Level1,
+        Level2,
+        Level3,
+        Level4
+    };
+
+    public Charm[] GetCharms(CharmList list)
+    {
+        switch (list)
+        {
+            case CharmList.Level1: return Level1;
+            case CharmList.Level2: return Level2;
+            case CharmList.Level3: return Level3;
+            case CharmList.Level4: return Level4;
+            default: return null;
+        }
+    }
+}
